3ed08d52317bd12a9dcf06ff1ec3ee27f7f345d1
Bridget Turnbull <bturnbull@Bridgets-MacBook-Pro.local>
2019-11-20 14:47:02 +1100

    Add filtering to query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,16 @@
-const feed = (parent, args, context) => {
-    return context.prisma.burgers();
-};
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            {description_contains: args.filter},
+            {name_contains: args.filter},
+            {restaurant_contains: args.filter},
+        ],
+    } : {};
+    const burgers = await context.prisma.burgers({
+        where
+    });
+    return burgers
+}
 
 module.exports = {
     feed
